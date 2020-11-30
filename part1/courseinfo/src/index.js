29383d0952056d91766179b8398f61c2537ffd0e
HawkBrave <doganyigityilmazz@gmail.com>
2020-11-30 03:09:39 +0300

    exercise

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,27 +1,38 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Hello = (props) => {
+const Content = (props) => (
+  <div>
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  </div>
+);
+
+const Total = (props) => (
+  <div>
+    <p>Number of exercises {props.total}</p>
+  </div>
+);
+
+const App = () => {
+  const course = 'Half Stack application development';
+  const part1 = 'Fundamentals of React';
+  const exercises1 = 10;
+  const part2 = 'Using props to pass data';
+  const exercises2 = 7;
+  const part3 = 'State of a component';
+  const exercises3 = 14;
+
   return (
     <div>
-      <p>
-        Hello {props.name}, you are {props.age} years old
-      </p>
+      <h1>{course}</h1>
+      <Content part={part1} exercises={exercises1} />
+      <Content part={part2} exercises={exercises2} />
+      <Content part={part3} exercises={exercises3} />
+      <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   )
 }
 
-const App = () => {
-  const name = 'Peter'
-  const age = 10
-
-  return (
-    <>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
-    </>
-  )
-}
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'))
