13d014b3821bb55f550325cd3be55af24a45e49a
HawkBrave <doganyigityilmazz@gmail.com>
2020-12-01 23:30:10 +0300

    added counter

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 import _ from 'lodash'
 
@@ -8,8 +8,8 @@ const Part = props => (
   </p>
 )
 
-const Content = props => {
-  let parts = _.zip(props.names, props.exercises).map(val => (
+const Content = ({names, exercises}) => {
+  let parts = _.zip(names, exercises).map(val => (
     <Part name={val[0]} exercise={val[1]} />
   ));
   return (
@@ -17,8 +17,7 @@ const Content = props => {
       {parts}
     </div>
   );
-};
- 
+}
 
 const Total = props => (
   <div>
@@ -26,7 +25,17 @@ const Total = props => (
   </div>
 );
 
+const Button = props => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const Display = props => <h3>{props.counter}</h3>
+
 const App = () => {
+  const [counter, setCounter] = useState(0);
+
   const course = 'Half Stack application development';
   const part1 = 'Fundamentals of React';
   const exercises1 = 10;
@@ -35,11 +44,18 @@ const App = () => {
   const part3 = 'State of a component';
   const exercises3 = 14;
 
+  const inc = () => setCounter(counter + 1)
+
   return (
     <div>
       <h1>{course}</h1>
-      <Content names={[part1, part2, part3]} exercises={[exercises1, exercises2, exercises3]} />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Display counter={counter}/>
+      <Content names={[part1, part2, part3]} exercises={[exercises1, exercises2, exercises3]}/>
+      <Total total={exercises1 + exercises2 + exercises3}/>
+      <Button 
+        handleClick={inc}
+        text={"increment counter"}
+      />
     </div>
   )
 }
