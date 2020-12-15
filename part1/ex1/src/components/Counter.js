32ccec5aba384b87acd12e0b676c182da7e0b9ef
HawkBrave <doganyigityilmazz@gmail.com>
2020-12-15 16:05:36 +0300

    general improvements

--- part1/ex1/src/components/Counter.js
+++ part1/ex1/src/components/Counter.js
@@ -19,25 +19,38 @@ const Wrapper = ({className, children}) => {
 }
 
 
-const Button = ({id, handleClick, text}) => (
-  <button className="button" id={id} onClick={handleClick}>
-    {text}
-  </button>
-)
+const Button = ({id, handleClick, text}) => {
+  const sheet = document.styleSheets[0];
+  const wrapper = function (f) {
+    return function (e) {
+      let rect = e.target.getBoundingClientRect();
+      let x = e.clientX - rect.left; //x position within the element.
+      let y = e.clientY - rect.top;
+      sheet.cssRules[5].style.left = `${x}px`;
+      sheet.cssRules[5].style.top = `${y}px`;
+      return f();
+    }
+  }
+  return (
+    <button className="button" id={id} onClick={wrapper(handleClick)}>
+      {text}
+    </button>
+  )
+}
 
 const Counter = (props) => {
   const [counter, setCounter] = useState(0);
   const [allClicks, setAll] = useState([]);
 
-  const incr = () => {
+  const incr = function () {
     setAll(allClicks.concat('inc '));
     setCounter(counter + 1);
   }
-  const decr = () => {
+  const decr = function () {
     setAll(allClicks.concat('decr '));
     setCounter(counter > 0 ? counter - 1 : 0);
   }
-  const setzero = () => {
+  const setzero = function () {
     setAll([]);
     setCounter(0);
   }
--- part2/ex1/src/App.js
+++ part2/ex1/src/App.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import {React, useState} from 'react';
 import ReactDOM from 'react-dom';
 import Note from './components/Note';
 
-const App = ({notes}) => {
+
+
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState('');
+  const [showAll, setShowAll] = useState(true);
+
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    }
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value)
+  }
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important === true);
+
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all' }
+        </button>
+      </div>
       <ul>
         {
           notes.map(note => 
@@ -13,6 +45,13 @@ const App = ({notes}) => {
           )
         }
       </ul>
+      <form onSubmit={addNote}>
+        <input 
+          value={newNote} 
+          onChange={handleNoteChange}
+        />
+        <button type="submit">save</button>
+      </form>
     </div>
   );
 }
